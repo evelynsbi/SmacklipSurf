6b2aef01de36ed4f7c6b8b980f70d2fd78bf07ea
orjanhsy <orjanhsy@gmail.com>
2024-03-12 15:57:00 +0100

    implement getRelevantAlerts for polygons

--- project/app/src/main/java/com/example/myapplication/data/metalerts/MetAlertsRepository.kt
+++ project/app/src/main/java/com/example/myapplication/data/metalerts/MetAlertsRepository.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication.data.metalerts
 
+import androidx.compose.ui.geometry.CornerRadius
+import com.example.myapplication.model.SurfArea
 import com.example.myapplication.model.metalerts.Features
 
 interface MetAlertsRepository{
@@ -16,49 +18,36 @@ class MetAlertsRepositoryImpl (
     }
 
     override suspend fun getFeaturesHoddevik(): List<Features> {
-        // Coordinates for Hoddevik
-        val hoddevikLatitude = 62.723
-        val hoddevikLongitude = 5.103
 
-        // Define a bounding box with approximately a 10 km radius around Hoddevik
-        val radiusDegrees = 0.1
-        val minLatitudeHoddevik = hoddevikLatitude - radiusDegrees
-        val maxLatitudeHoddevik = hoddevikLatitude + radiusDegrees
-        val minLongitudeHoddevik = hoddevikLongitude - radiusDegrees
-        val maxLongitudeHoddevik = hoddevikLongitude + radiusDegrees
-
-        val hoddevikLatLongArea = listOf(
-            minLatitudeHoddevik,
-            maxLatitudeHoddevik,
-            minLongitudeHoddevik,
-            maxLongitudeHoddevik
-        )
         // Fetch all features
         val allFeatures: List<Features> = metAlertsDataSource.fetchMetAlertsData().features
+        return allFeatures
+    }
 
-        fun inArea(lat: Double, long: Double): Boolean {
-            return (lat in hoddevikLatLongArea[0]..hoddevikLatLongArea[1] && long in hoddevikLatLongArea[2]..hoddevikLatLongArea[1])
-        }
+    private fun inArea(lat: Double, long: Double, surfArea: SurfArea, radius: Double = 0.1): Boolean {
+        return (
+                lat in surfArea.lat - radius..surfArea.lat + radius &&
+                        long in surfArea.long - radius..surfArea.long + radius
+                )
+    }
 
-        fun getRelevantAlertsFor(surfArea: SurfArea): List<String> {
-            val relevantAlerts: MutableList<String> = mutableListOf()
-            allFeatures.forEach() {feature ->
-                val coordinates = feature.geometry?.coordinates
-                if (feature.geometry?.type == "Polygon") {
-                    coordinates?.forEach {i ->
-                        i.forEach { j ->
-                            val lat = j[0] as Double
-                            val long = j[1] as Double
-                            if (inArea(lat, long)) {
-                                feature.properties?.area?.let { relevantAlerts.add(it) }
-                            }
+    fun getRelevantAlertsFor(surfArea: SurfArea, allFeatures: List<Features>): List<String> {
+        val relevantAlerts: MutableList<String> = mutableListOf()
+        allFeatures.forEach() {feature ->
+            val coordinates = feature.geometry?.coordinates
+            if (feature.geometry?.type == "Polygon") {
+                coordinates?.forEach {i ->
+                    i.forEach { j ->
+                        val lat = j[0] as Double
+                        val long = j[1] as Double
+                        if (inArea(lat, long, surfArea)) {
+                            feature.properties?.area?.let { relevantAlerts.add(it) }
                         }
                     }
                 }
             }
-            return relevantAlerts
         }
+        return relevantAlerts
     }
-
 
 }
