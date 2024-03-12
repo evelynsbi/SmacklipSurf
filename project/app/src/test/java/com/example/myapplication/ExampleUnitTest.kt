97bddcaa46d2b81e9590bfd15f1fd1315187a6b0
orjanhsy <orjanhsy@uio.no>
2024-03-12 19:16:25 +0100

    Add get relevant alerts test for local json

--- project/app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ project/app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.runBlocking
 import com.example.myapplication.data.oceanforecast.HoddevikDataSourceDataSource
 import com.example.myapplication.data.oceanforecast.HoddevikRepository
 import com.example.myapplication.model.SurfArea
+import com.example.myapplication.model.metalerts.Features
 import com.example.myapplication.model.oceanforecast.Data
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
@@ -24,22 +25,12 @@ import java.io.File
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    //global
+    private val gson = Gson()
+
+    //MetAlerts
     private val metAlertsRepository: MetAlertsRepositoryImpl = MetAlertsRepositoryImpl()
-
-    private val hoddevikDataSourceDataSource = HoddevikDataSourceDataSource()
-    private val hoddevikRepository = HoddevikRepository(hoddevikDataSourceDataSource)
-
-
-    @Test
-    fun addition_isCorrect() = runBlocking{
-
-        val timeSeries: List<Pair<String, Data>> = hoddevikRepository.getTimeSeries()
-        val time1 = timeSeries.get(0).first
-        //val data1 = timeSeries.get(0).second
-        //assertEquals("2024-03-07T13:00:00Z", time1)
-        println("$time1 --------------hei----------------")
-
-    }
+    private val metAlertJson = File("src/test/java/com/example/myapplication/metAlerts.json").readText()
     @Test
     fun testMetAlertsAreaNameWithProxy() = runBlocking {
         val feature = metAlertsRepository.getFeatures()[0]
@@ -55,7 +46,29 @@ class ExampleUnitTest {
     }
 
     @Test
-    fun getRelevantAlertsForNordkapp() {
+    fun getRelevantAlertsForNordkappOnlyGetsAlertsFromAreaNordkapp() {
+        println("Script is ran from: ${System.getProperty("user.dir")}")
+        val metAlerts: MetAlerts = gson.fromJson(metAlertJson, MetAlerts::class.java)
+        val features = metAlerts.features
+        val relevantAlerts = metAlertsRepository.getRelevantAlertsFor(SurfArea.NORDKAPP, features)
+        relevantAlerts.forEach { assert(it.properties?.area?.lowercase()?.contains("nordkapp") == true) }
+    }
+
+
+    //Ocean forecast
+    private val hoddevikDataSourceDataSource = HoddevikDataSourceDataSource()
+    private val hoddevikRepository = HoddevikRepository(hoddevikDataSourceDataSource)
+
+
+    @Test
+    fun addition_isCorrect() = runBlocking{
+
+        val timeSeries: List<Pair<String, Data>> = hoddevikRepository.getTimeSeries()
+        val time1 = timeSeries.get(0).first
+        //val data1 = timeSeries.get(0).second
+        //assertEquals("2024-03-07T13:00:00Z", time1)
+        println("$time1 --------------hei----------------")
 
     }
+
 }
