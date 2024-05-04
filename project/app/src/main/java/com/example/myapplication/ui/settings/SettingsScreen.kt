3efe17883221ef33dc2e4f816c00454bc591f9d7
munaisman <munaai@uio.no>
2024-05-04 11:11:34 +0200

    new features in screen

--- project/app/src/main/java/com/example/myapplication/ui/settings/SettingsScreen.kt
+++ project/app/src/main/java/com/example/myapplication/ui/settings/SettingsScreen.kt
@@ -3,16 +3,18 @@ package com.example.myapplication.ui.settings
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -20,7 +22,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -53,76 +54,91 @@ fun SettingsScreen(navController: NavController, settingsViewmodelFactory: Setti
     //val navController = NavigationManager.navController
     AppTheme(darkTheme = isDarkThemeEnabled) {
         Scaffold(
-            topBar = {
-                CenterAlignedTopAppBar(
-                    colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        titleContentColor = MaterialTheme.colorScheme.primary,
-                    ),
-                    title = {
-                        Text(text = "Settings")
-                    })
-            },
             bottomBar = {
                 BottomBar(navController = navController)
             }
         ) { innerPadding ->
-            // SmackLip logo
-            LazyColumn(
+            Column(
                 modifier = Modifier
-                    .fillMaxSize(),
+                    .padding(innerPadding)
             ) {
-                item {
-                    Image(
-                        painter = painterResource(id = R.drawable.smacklip_logo),
-                        contentDescription = "app logo",
+                Box(modifier = Modifier.fillMaxSize()){
+                    Column(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp)
-                    )
-                }
-                // Kort for dark mode
-                item {
-                    Card(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp)
+                            .fillMaxSize(),
                     ) {
-                        var expandedThemeCard by remember { mutableStateOf(false) }
-                        Column(modifier = Modifier.padding(12.dp)) {
-                            Text(text = "Velg app tema", style = MaterialTheme.typography.headlineMedium)
-                            IconButton(onClick = { expandedThemeCard = !expandedThemeCard }) {
-                                Icon(Icons.Default.ExpandMore, contentDescription = "Utvid")
-                            }
-                            // Expand theme card
-                            if (expandedThemeCard){
-                                Row(
+                        Image(
+                            painter = painterResource(id = R.drawable.smacklip_logo),
+                            contentDescription = "app logo",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        )
+                        Spacer(modifier = Modifier.height(14.dp))
+                    }
+                    Column(
+                    ) {
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            //horizontalAlignment = Arrangement.Center
+                        ) {
+                            item {
+                                Card(
                                     modifier = Modifier
                                         .fillMaxWidth()
-                                        .padding(8.dp),
-                                    horizontalArrangement = Arrangement.SpaceBetween
-                                ){
-                                    Text(text = if (isDarkThemeEnabled) "Dark Mode" else "Light Mode")
-                                    Switch(
-                                        checked = isDarkThemeEnabled,
-                                        onCheckedChange = {isChecked ->
-                                            settingsScreenViewModel.updateTheme(if (isChecked) Settings.Theme.DARK else Settings.Theme.LIGHT)
+                                        .padding(8.dp)
+                                ) {
+                                    var expandedThemeCard by remember { mutableStateOf(false) }
+                                    Column(modifier = Modifier.padding(12.dp)) {
+                                        Text(
+                                            text = "Velg app tema",
+                                            style = MaterialTheme.typography.headlineMedium
+                                        )
+                                        IconButton(onClick = {
+                                            expandedThemeCard = !expandedThemeCard
+                                        }) {
+                                            Icon(
+                                                Icons.Default.ExpandMore,
+                                                contentDescription = "Utvid"
+                                            )
                                         }
-                                    )
+                                        // Expand theme card
+                                        if (expandedThemeCard) {
+                                            Row(
+                                                modifier = Modifier
+                                                    .fillMaxWidth()
+                                                    .padding(8.dp),
+                                                horizontalArrangement = Arrangement.SpaceBetween
+                                            ) {
+                                                Text(text = if (isDarkThemeEnabled) "Dark Mode" else "Light Mode")
+                                                Switch(
+                                                    checked = isDarkThemeEnabled,
+                                                    onCheckedChange = { isChecked ->
+                                                        settingsScreenViewModel.updateTheme(if (isChecked) Settings.Theme.DARK else Settings.Theme.LIGHT)
+                                                    }
+                                                )
+                                            }
+                                        }
+
+                                    }
                                 }
+                            }
+                            item{
+                                InformationCard(title = "Hvorfor SmackLip Surf?", content = "Beskrivelse")
+                            }
+                            item {
+                                InformationCard(title = "Hvor henter vi data fra?", content = "Beskrivelse")
                             }
 
                         }
 
+
+                    }
+
                     }
                 }
-                //Info kort
-                item{
-                    InformationCard(title = "Hvorfor SmackLip Surf?", content = "Beskrivelse")
-                }
-                item {
-                    InformationCard(title = "Hvorfor SmackLip Surf?", content = "Beskrivelse")
-                }
+
             }
 
         }
@@ -131,7 +147,7 @@ fun SettingsScreen(navController: NavController, settingsViewmodelFactory: Setti
     }
 
 
-}
+
 
 @Composable
 fun InformationCard(title: String, content: String) {
